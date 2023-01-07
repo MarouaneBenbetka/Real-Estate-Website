2f7d5873c91acd7ddd3d708d719aa91cd9adb3d7
RayaneA7 <ka_kebir@esi.dz>
2023-01-07 20:18:33 +0100

    Add registration with google modal to get additional infos like address and phone number

--- components/shared/Layout.jsx
+++ components/shared/Layout.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import GoggleRegisterForm from "../formComponents/GoggleRegisterForm"
+import { useEffect, useState } from "react";
 
 function Layout({ children }) {
+
+	const [isNewUser, setIsNewUser] = useState(null);
+
 	return (
 		<div className="text-dark-blue font-libre-franklin">
+			      <input
+        type="checkbox"
+        id="my-modal5"
+        defaultChecked={isNewUser !== null ? !isNewUser: false}
+        className="modal-toggle"
+      />
+      <div className="modal hero lg:min-w-xl" id="my-modal-5">
+        <div className="">
+          <GoggleRegisterForm />
+        </div>
+      </div>
 			<div>
 				<Navbar />
 			</div>
