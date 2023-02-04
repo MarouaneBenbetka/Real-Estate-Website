f12b064884505e2a90ed826df23d0d41e4dd3d3c
RayaneA7 <ka_kebir@esi.dz>
2023-02-04 17:01:29 +0100

    deployement fix eslint

--- components/Home/AnnouceCard.jsx
+++ components/Home/AnnouceCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createStyles, Image, Card, Text, Group, Button } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
--- pages/Annonces/[annonceId].jsx
+++ pages/Annonces/[annonceId].jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react-hooks/exhaustive-deps */
 import Link from "next/link";
 import { useRouter } from "next/router";
